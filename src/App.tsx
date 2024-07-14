6822af9f52793d476b4e521109c85696f556858a
Naincy <naincyk15042004@gmail.com>
2024-07-14 12:06:08 +0530

    set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-function App(){
-  return <h1>Hii</h1>
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { NewNote } from "./NewNote";
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
 }
-export default App
+
+export type Tag = {
+  id: string
+  label: string
+}
+
+function App() {
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+export default App;
